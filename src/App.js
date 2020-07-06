2240b529dcd004655a993f2e14ca98e6c34bad64
Gibum Kang <Gibum.Kang@creditone.com>
2020-07-06 07:30:18 -0700

    cleaned master branch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import "./App.css";
-import TimesList from "./components/TimesList";
-import AddTimeForm from "./components/AddTimeForm";
-import Header from "./components/Header";
-import Signup from "./components/Signup";
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <h1>Just Clock It</h1>
-            <TimesList />
-            <AddTimeForm />
-            <Signup />
+            You are in the master branch (Which is empty!).
         </div>
     );
 }
