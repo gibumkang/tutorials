c69988da357a59d757a8ec3cdd2051199009f9b3
ben <gibumkang@gmail.com>
2020-08-12 21:22:23 -0700

    Restarting kouple project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import TimesList from "./components/TimesList";
-import AddTimeForm from "./components/AddTimeForm";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Just Clock It</h1>
-            <TimesList />
-            <AddTimeForm />
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
